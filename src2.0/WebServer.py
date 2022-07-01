2c611b4465e33a4a708ed311bfdce0baa298ce0a
ShiWenber <1210169842@qq.com>
2022-07-01 20:44:12 +0800

    1

--- src2.0/WebServer.py
+++ src2.0/WebServer.py
@@ -9,145 +9,11 @@ from sqlite3 import connect
 from tkinter.tix import Tree  # 正则
 import os
 
+from request import Request
+
 
 # 设置静态文件根目录
 HTML_ROOT_DIR = "."
-
-
-def entity_body(request_message):
-    """提取实体主体
-
-    Args:
-        request_message (bytes): 请求报文
-
-    Returns:
-        bytes: 实体主题
-    """
-    # 提取实体主体
-    entity_body = request_message.split(b"\r\n\r\n")[1]  # 切割出实体主体
-    return entity_body
-
-# 将username=shi&password=sjdif&sex=male字符串转化为字典
-
-
-def to_postdict(entity_body):
-    """将entity_body字符串转化为字典类型
-
-    Args:
-        entity_body (bytes str):实体主体
-
-    Returns:
-        dict:实体主体键值对
-    """
-    # 如果只有一个&，那么就判断为单键值对
-    res = {}
-    if entity_body.find(b"&") == -1 and entity_body.find(b"=") == -1:
-        print("entity_body 非键值对形式")
-        raise Exception("entity_body 非键值对形式")
-    if entity_body.find(b"&") == -1:
-        # 单键值对
-        key = entity_body.split(b"=")[0]
-        value = entity_body.split(b"=")[1]
-        res[key] = value
-        return res
-    else:
-        # 多键值对
-        key_values = entity_body.split(b'&')
-        for key_value in key_values:
-            key, value = key_value.split(b'=')
-            res[key] = value
-        return res
-
-
-def do_post(client, request_message, url):
-    """post报文处理
-
-    Args:
-        client (socket.socket): socket.socket对象
-        request_message (bytes): 请求报文
-        url (bytes): 请求url
-    """
-    print(url)
-    response_start_line = bytes("", "utf-8")
-    response_header = bytes("", "utf-8")
-    response_body = bytes("", "utf-8")
-    try:
-        # req_datas =
-        print("post接受的请求数据：\n", request_message)
-        # 用正则表达式提取request_message报文中的实体主体
-
-        if url == b"/test":
-            print("test")
-            response_start_line = bytes("HTTP/1.1 404 Not Found\r\n", "utf-8")
-            response_header = bytes("Server:My server\r\n", "utf-8")
-            response_body = bytes("test", "utf-8")
-        elif url == b"/print":
-            response_start_line = bytes("HTTP/1.1 200 OK\r\n", "utf-8")
-            response_header = bytes("Server:My server\r\n", "utf—8")
-            postdict = to_postdict(entity_body(request_message))
-            response_body = bytes(str(postdict), "utf-8")
-        if url == b"/download":
-            print("download")
-            response_start_line = bytes("HTTP/1.1 200 OK\r\n", "utf-8")
-            response_header = bytes("Server:My server\r\n", "utf—8")
-            response_body = bytes("download", "utf-8")
-    except Exception as e:
-        print(e)
-        response_start_line = bytes("HTTP/1.1 404 Not Found\r\n", "utf-8")
-        response_header = bytes("Server:My server\r\n", "utf-8")
-        response_body = bytes("there is something error!", "utf-8")
-
-        # 构造响应数据
-    response_data = response_start_line + response_header + \
-        bytes("\r\n", "utf_8") + response_body
-    # print("返回数据：", response_data)  # 打印一下返回数据
-
-    # 向客户端返回响应数据
-    client.send(response_data)
-
-    # 关闭客户端连接
-    # client.close()
-
-
-def do_get(client, request_message, url, file_name):
-    """get报文处理
-
-    Args:
-        client (socket.socket): socket.socket对象
-        request_message (bytes): 请求报文
-        url (bytes): 请求url
-    """
-    print(url)
-    response_start_line = bytes("", "utf-8")
-    response_header = bytes("", "utf-8")
-    response_body = bytes("", "utf-8")
-    if "/" == file_name:
-        file_name = "/index.html"
-
-        # 打开文件，读取内容
-    try:
-        file = open(HTML_ROOT_DIR + file_name, "rb")
-    except IOError:
-        response_start_line = bytes("HTTP/1.1 404 Not Found\r\n", "utf-8")
-        response_header = bytes("Server:My server\r\n", "utf-8")
-        response_body = bytes("The file is not found!", "utf-8")
-    else:
-        file_data = file.read()
-        file.close()
-        response_body = file_data
-        response_start_line = bytes("HTTP/1.1 200 OK\r\n", "utf_8")
-        response_header = bytes("Server:My server\r\n", "utf—8")
-        response_header += bytes("Content-Length:%d\r\n" %
-                                 len(response_body), "utf-8")
-    # 构造响应数据
-    response_data = response_start_line + response_header + \
-        bytes("\r\n", "utf_8") + response_body
-    # print("返回数据：", response_data)  # 打印一下返回数据
-    # 向客户端返回响应数据
-    print("响应报文：", response_data)
-    client.send(response_data)
-    # 关闭客户端连接
-    # client.close()
 
 
 def process_connection(client):
@@ -161,44 +27,17 @@ def process_connection(client):
     print("新连接：", client.getpeername())
     # 接受客户端发来的数据
     # client.settimeout(5) # 设置recv的超时时间
-    request_message = client.recv(1024)
     # client.settimeout(None)
+
+    req = Request(client)
     # 打印接受的数据
-    print("接受的请求数据：", request_message)
+    print("接受的请求数据：", req.get_request_message)
+    print("请求完整路径：", HTML_ROOT_DIR + req.get_file_name)
 
-    # # -------------
-
-    # 请求 message(报文)
-
-    # - request line(请求行): method(GET POST HEAD PUT DELETE), URL, protocol
-    # - header line(首部行):
-    # - entity body:
-    # # -------------
-
-    # 处理请求
-    req_mes_lines = request_message.splitlines()
-
-    request_line = req_mes_lines[0]  # GET /index.html HTTP/1.1 请求行
-    url = request_line.split()[1]  # /index.html 获取相对url
-    header_lines = req_mes_lines[1:]  # 首部行
-
-    print(req_mes_lines[0])
-    print(req_mes_lines[3])
-    length = req_mes_lines[3].decode("utf-8").split(":")
-    print(length[1])
-
-    # 提取用户请求的文件名
-    file_name = re.match(r"\w+ +(/[^  ]*) ",
-                         request_line.decode("utf_8")).group(1)
-    # 请求方法
-    method = re.match(r"(\w+) +/[^ ]* ", request_line.decode("utf-8")).group(1)
-
-    print("请求完整路径：", HTML_ROOT_DIR + file_name)
-
-    if method == "GET":
-        do_get(client, request_message, url, file_name)
-    elif method == "POST":
-        do_post(client, request_message, url)
+    if req.get_method == "GET":
+        req.do_get()
+    elif req.get_method == "POST":
+        req.do_post()
 
 
 def main():
